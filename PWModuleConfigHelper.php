b83d864914a22f2c466cb7a77ec6120b624c6cc6
owzim <nope@nope.nope>
2014-06-03 04:23:38 +0200

    Initial commit

---
+++ PWModuleConfigHelper.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * Module Config Helper for ProcessWire
+ *
+ * @author Christian Raunitschka (owzim)
+ * @copyright Christian Raunitschka
+ * @version 0.0.1
+ * <https://github.com/owzim/PWModuleConfigHelper>
+ *
+ * See README.md for usage
+ *
+ */
+
+class PWModuleConfigHelper {
+
+    const DEFAULT_FIELDTYPE = 'InputfieldText';
+    const DEFAULT_OPTIONS_FIELDTYPE = 'InputfieldRadios';
+
+    public static function apply($module, $config) {
+        foreach ($config as $key => $value) {
+            $module->set($key, $value['value']); // set default value in construct
+        }
+    }
+
+    public static function renderForm($data, $config) {
+
+        $modules = wire('modules');
+
+        $data = (object) $data; // cast to object, because it's prettier to acces keys
+        $config = (object) $config; // cast to object, because it's prettier to acces keys
+
+        foreach ($config as $key => $valueData) {
+            if(!isset($data->$key)) {
+                $data->$key = $valueData->value;
+            }
+        }
+
+        $form = new InputfieldWrapper();
+
+        foreach ($config as $key => $valueData) {
+
+            $valueData = (object) $valueData; // cast to object, because it's prettier to acces keys
+            $value = $valueData->value;
+            $label = isset($valueData->label) ? $valueData->label : $key;
+
+            if (isset($valueData->options)) {
+                $inputFieldType = isset($valueData->inputFieldtype) ? $valueData->inputFieldtype : self::DEFAULT_OPTIONS_FIELDTYPE;
+            } else {
+                $inputFieldType = isset($valueData->inputFieldtype) ? $valueData->inputFieldtype : self::DEFAULT_FIELDTYPE;
+            }
+
+            $f = $modules->get($inputFieldType);
+            $f->name = $key;
+            $f->label = $label;
+
+            if (isset($valueData->options)) {
+                foreach ($valueData->options as $optionLabel => $optionValue) {
+                    $f->addOption($optionValue, $optionLabel);
+                }
+            }
+
+            if (isset($valueData->attributes)) {
+                foreach ($valueData->attributes as $attributeKey => $attributeValue) {
+                    $f->set($attributeKey, $attributeValue);
+                }
+            }
+
+            if(isset($data->$key)) {
+                $f->value = $data->$key;
+            } else {
+                $f->value = $value;
+            }
+
+            $form->add($f);
+        }
+
+        return $form;
+    }
+}
---
+++ README.md
@@ -0,0 +1,93 @@
+# Module Config Helper for ProcessWire
+
+## Usage
+
+### Define a default array in your module:
+
+```php
+protected static $defaultConfig = array(
+
+    // example with just one option
+    'prettySetting'  => array(
+
+        // the label for the form
+        'label' => 'Pretty Setting',
+
+        // the default value
+        'value' => 'I am pretty',
+
+        // optional, defaults to 'InputfieldText'
+        'inputFieldType' => 'InputfieldText',
+
+        // set any additional attribute to the input field
+        "attributes" => array(
+            "optionColumns" => 1
+        )
+    ),
+
+    // example with multiple options
+    'awesomeSetting' => array(
+
+        // the label for the form
+        'label' => 'Awesome Setting',
+
+        // the default value
+        'value' => 2,
+
+        // optional, defaults to 'InputfieldRadios'
+        'inputFieldType' => 'InputfieldRadios',
+
+        // each key is for the form label, each value will be saved if selected
+        'options' => array(
+            'Option 1' => 1,
+            'Option 2' => 2,
+            'Option 3' => 3
+        ),
+
+        // set any additional attribute to the input field
+        "attributes" => array(
+            "optionColumns" => 1
+        )
+    )
+);
+```
+
+### Apply the defaults to your module:
+
+```php
+public function __construct() {
+    PWModuleConfigHelper::apply($this, $defaultConfig);
+}
+```
+
+### Render out the form:
+
+```php
+public static function getPWModuleConfigInputfields(array $data) {
+    return PWModuleConfigHelper::renderForm($data, self::$defaultConfig);
+}
+```
+
+#### Result:
+
+![screenshot](http://i.imgur.com/gaxRce8.png)
+
+
+
+### Access any of the config settings from your module:
+
+```php
+$this->awesomeSetting;
+```
+
+### Version history
+
+* v0.0.1 initial version
+
+### ProcessWire
+
+[processwire.com](http://processwire.com)
+
+### License
+
+This software is licensed under [The MIT License (MIT)](http://opensource.org/licenses/MIT)
